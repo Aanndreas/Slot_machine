2f6b4e1ca78d59d10df871686de363f3f7a969fa
Aanndreas <andreas.lundin@hotmail.com>
2024-01-07 20:20:53 +0100

    :tada: npm package inst. + Temporary pseudo code

---
+++ App.js
@@ -0,0 +1,9 @@
+//PSEUDO CODE BELOW:
+// 1. Users shall be able to deposit money
+// 2. Users shall be able to determine the number of lines to bet on
+// 3. The program shall be able to collect a bet amount
+// 4. The program shall be able to spin slot machine
+// 5. The program shall be able to check if the user won
+// 6. The program shall be able to give the user the winnings or take their bet 
+// 7. The program shall be able to play again
+
